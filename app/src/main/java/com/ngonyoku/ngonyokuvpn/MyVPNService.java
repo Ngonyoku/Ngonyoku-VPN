e0c8923b0990a65f3c71dc2af55be688d4f0ce04
NGONYOKU <ngonyoku001@gmail.com>
2023-01-15 21:15:12 +0300

    Code written, still debugging

---
+++ app/src/main/java/com/ngonyoku/ngonyokuvpn/MyVPNService.java
@@ -0,0 +1,14 @@
+package com.ngonyoku.ngonyokuvpn;
+
+
+import android.net.VpnService;
+
+public class MyVPNService extends VpnService {
+    public MyVPNService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
